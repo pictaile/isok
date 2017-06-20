03a4a835e01d68b1fa17ca082be7739264fdacd3
Konstantin <kostya.ochotnik@gmail.com>
2017-06-20 18:59:35 +0300

    add entity

---
+++ src/StaticBundle/Entity/Messages.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace StaticBundle\Entity;
+
+/**
+ * Messages
+ */
+class Messages
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $user;
+
+    /**
+     * @var \DateTime
+     */
+    private $date = 'CURRENT_TIMESTAMP';
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     *
+     * @return Messages
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set user
+     *
+     * @param string $user
+     *
+     * @return Messages
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return string
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Messages
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+}
+

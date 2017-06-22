d4c59225f61c5069fa34b99fe176b6e120e3146c
AnnaLadyzhenskaya <ann.ladyzhenskaya@gmail.com>
2017-06-22 18:37:18 +0300

    add routing

--- src/StaticBundle/Controller/DefaultController.php
+++ src/StaticBundle/Controller/DefaultController.php
@@ -3,11 +3,18 @@
 namespace StaticBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Put;
 
 class DefaultController extends Controller
 {
+    /**
+     * GET Route annotation.
+     * @Get("/")
+     */
     public function indexAction()
     {
-        return $this->render('StaticBundle:Default:index.html.twig');
+        return $this->render('default/index.html.twig');
     }
 }

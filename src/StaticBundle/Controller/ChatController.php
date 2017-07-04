2508580edffabb853bb1a4bafe735a6c8f640423
Konstantin <kostya.ochotnik@gmail.com>
2017-07-04 18:52:22 +0300

    new item

--- src/StaticBundle/Controller/ChatController.php
+++ src/StaticBundle/Controller/ChatController.php
@@ -4,13 +4,32 @@ namespace StaticBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use FOS\RestBundle\Controller\Annotations\RouteResource;
+use FOS\RestBundle\Controller\Annotations\QueryParam;
+use FOS\RestBundle\Request\ParamFetcherInterface;
 use StaticBundle\Entity\Messages;
 
 class ChatController extends Controller
 {
-    public function postMessageAction()
-    {
-        $content = $this->getRequest()->getContent();
 
+    public function getUsersAction()
+    {
+        $request = Request::createFromGlobals();
+        $content = $request->getContent();
+
+
+        return new JsonResponse(['status' => 'susasssdasdasdccess']);
+    } // "post_users"    [GET] /users
+
+    public function postChatAction()
+    {
+        $request = Request::createFromGlobals();
+        $content = $request->getContent();
+        var_dump($content);
+
+        return new JsonResponse(['status' => 'success']);
     } // "post_users"    [POST] /users
+
+
 }
